63951a79b0a1f2976a5acaf0801c45f0c4b0dbd5
dhilipsiva <dhilipsiva@pm.me>
2019-07-12 13:15:38 +0530

    Implement BUMP_VERSION fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,15 @@
-rm -rf dist/
-bumpversion patch
-export CURRENT_BRANCH
+BUMP_VERSION=$1
+if [ "$BUMP_VERSION" = "" ]
+then
+    BUMP_VERSION="patch"
+fi
+
+bumpversion $BUMP_VERSION
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 git push --tags
 git push origin "$CURRENT_BRANCH:$CURRENT_BRANCH"
+
+rm -rf dist/
 python setup.py sdist
 python setup.py bdist_wheel
 twine upload dist/*
