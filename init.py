a6e8f26db825c4d4ac2cb8373b52e819b404abaa
dhilipsiva <dhilipsiva@pm.me>
2019-08-06 13:56:39 +0530

    Just print what we are awaiting :P

--- init.py
+++ init.py
@@ -7,13 +7,11 @@ from jatayu.user.models import User
 
 async def run():
     connection = await asyncpg.connect(
-        user='jatayu', password='jatayu', database='jatayu',
-        host='127.0.0.1')
+        user='jatayu', password='jatayu', database='jatayu', host='127.0.0.1')
     pg = PostgreSQL(connection)
-    __import__('ipdb').set_trace()
-    foo = await pg.create_table(User)
     values = await pg.create_extension_uuid_ossp()
-    print(values)
+    foo = await pg.create_table(User)
+    print(values, foo)
     await connection.close()
 
 loop = asyncio.get_event_loop()
