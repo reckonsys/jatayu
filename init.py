facdf4c37746ab471804ca7b32d576cc8f3b79e1
dhilipsiva <dhilipsiva@pm.me>
2019-10-10 18:47:36 +0530

    Update sample

--- init.py
+++ init.py
@@ -1,17 +1,22 @@
+# file: app.py
 import asyncio
 import asyncpg
 
 from pgorm.postgresql import PostgreSQL
-from jatayu.user.models import User
+from myapp.tables import User
 
 
 async def run():
     connection = await asyncpg.connect(
-        user='jatayu', password='jatayu', database='jatayu', host='127.0.0.1')
+        user='reckonsys', password='we ❤️ python',
+        database='demo', host='db.reckonsys.com')
     pg = PostgreSQL(connection)
-    values = await pg.create_extension_uuid_ossp()
-    foo = await pg.create(User)
-    print(values, foo)
+    # Lets create UUID OSSP extention (because UUIDField)
+    await pg.create_extension_uuid_ossp()
+    await pg.create(User)  # Create Table
+    user = await pg.insert(User(name='dhilipsiva', age=30))
+    print(user)
+    # User(pk=UUID('f46863...'), name='dhilipsiva', age=30)
     await connection.close()
 
 loop = asyncio.get_event_loop()
--- jatayu/user/models.py
+++ jatayu/user/models.py
@@ -1,10 +1,13 @@
+# file: myapp/tables.py
+from uuid import UUID, uuid4
 from dataclasses import dataclass
+
 from pgorm.resources.table import Table
-from pgorm.fields import BigIntField, CharField
+from pgorm.fields import TextField, SmallIntField, UUIDField
 
 
 @dataclass
 class User(Table):
-    cf: str = CharField(default="DASJFH")
-    bif: int = BigIntField()
-    dfcf: str = CharField(default_factory=lambda: 'WOOT!!!')
+    pk: UUID = UUIDField(default_factory=uuid4)
+    name: str = TextField()
+    age: int = SmallIntField()
